e26c27a4b1fb4226330ddc0ba2a63ceb8892cec5
Pravallika Boyapati <162458515+Pravallika0730@users.noreply.github.com>
2025-11-10 10:25:33 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ with col2:
         with tab1:
             st.image(result_img, caption="✅ Analysis Complete", use_column_width=True)
             st.success("Detection Complete!")
-            st.ballons()
+            st.snow()
 
             # Detected classes
             st.subheader("📋 Detected Classes:")
